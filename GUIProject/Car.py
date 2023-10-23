4e9faa50a1bf7cd5d852d8bb8a9706957fca2170
JavierCetinaValdez2007 <jcetinavaldez442@lsr7.net>
2023-10-23 09:43:46 -0500

    Fixing Car Movement

--- GUIProject/Car.py
+++ GUIProject/Car.py
@@ -1,54 +1,30 @@
 import pygame 
-
+from random import randrange
 class Car(pygame.sprite.Sprite):
     def __init__(self, STARTX, STARTY, SCREEN_WIDTH, SCREEN_HEIGHT):
         super().__init__()
+        self.t = 0
         self.width = 30
         self.height = 20
         self.image = pygame.Surface((self.width, self.height))
         self.end = (SCREEN_WIDTH)
-        self.speed = 10
+        self.speed = 1
         #color
         self.image.fill((0,0,0))
 
         self.SCREEN_WIDTH = SCREEN_WIDTH
         self.SCREEN_HEIGHT = SCREEN_HEIGHT
-
-
-
-        startX = STARTX
         startY = STARTY
-
-
-        self.rect = self.image.get_rect(
-            center = (startX, startY)
-        )
+        startX = STARTX
 
         self.pos = pygame.math.Vector2(startX,startY)
         self.center_pos = pygame.math.Vector2(
-            startX + (self.width),
+            startX + (self.width/2),
             startY + (self.height/2)
-            
         )
-    def update(self, keys):
-        up = keys[pygame.K_w] 
-        down = keys[pygame.K_s]
-        left = keys[pygame.K_a]
-        right = keys[pygame.K_d]
-
-        move = pygame.math.Vector2(right - left, down - up)
-
-        if move.length_squared() > 0:
-            move.scale_to_length(self.speed)
-            self.pos += move
-            self.rect.topleft = round(self.pos.x), round(self.pos.y)
-            
-
-        if self.rect.left < 0:
-            self.rect.left = 5
-        if self.rect.right > self.SCREEN_WIDTH:
-            self.rect.right = self.SCREEN_WIDTH-5
-        if self.rect.top <= 0:
-            self.rect.top = 5
-        if self.rect.bottom >= self.SCREEN_HEIGHT:
-            self.rect.bottom = self.SCREEN_HEIGHT-5
+        self.rect = self.image.get_rect(
+            center = (startX/2,startY/2)
+        )
+        if self.pos.x < SCREEN_WIDTH:
+            self.pos.x += 40
+        
--- GUIProject/main.py
+++ GUIProject/main.py
@@ -1,20 +1,25 @@
 import pygame
 from Player import Player
 from Car import Car
-import random
+from random import randrange
 def main():
    
     SCREEN_WIDTH = 1000
     SCREEN_HEIGHT = 600
-    x = random.randint(7,10)
-    y = random.randint(5,8)
-    z = random. randint(4,7)
+    x = randrange(5,6)
+    y = randrange(3,4)
+    z = randrange(1,2)
+
+    beginning = 0
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT,SCREEN_WIDTH,SCREEN_HEIGHT)
-    car1 = Car(SCREEN_WIDTH/30,SCREEN_HEIGHT/x,SCREEN_WIDTH,SCREEN_HEIGHT)
-    car2 = Car(SCREEN_WIDTH/30,SCREEN_HEIGHT/y*2,SCREEN_WIDTH,SCREEN_HEIGHT)
-    car3 = Car(SCREEN_WIDTH/30,SCREEN_HEIGHT/z*3,SCREEN_WIDTH,SCREEN_HEIGHT)
+    car1 = Car(beginning,SCREEN_HEIGHT/x,SCREEN_WIDTH,SCREEN_HEIGHT)
+    car2 = Car(beginning,SCREEN_HEIGHT/y,SCREEN_WIDTH,SCREEN_HEIGHT)
+    car3 = Car(beginning,SCREEN_HEIGHT/z,SCREEN_WIDTH,SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    end = (SCREEN_WIDTH)
+    car1.end = (SCREEN_WIDTH)
+    car2.end = (SCREEN_WIDTH)
+    car3.end = (SCREEN_WIDTH)
     clock = pygame.time.Clock()
     running = True
 
@@ -30,10 +35,10 @@ def main():
                 running = False
         pressedKeys = pygame.key.get_pressed()
         player.update(pressedKeys)
-        car1.update(pressedKeys)
         screen.fill((255,255,255))
         allSprites.draw(screen)
         pygame.display.flip()
+            
         if(pygame.sprite.collide_rect(player,car1)) or (pygame.sprite.collide_rect(player,car2)) or (pygame.sprite.collide_rect(player,car3)):
             running = False
         clock.tick(30)
